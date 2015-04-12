b6d415f53427fda73d4706907293e63981ad61da
Adam Nicholls <inkysplat@gmail.com>
2015-04-12 21:29:39 +0100

    got PHPUnit.xml working

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 12/04/15
+ * Time: 21:26
+ */
+
+/**
+ * Class CalculatorTest
+ */
+class CalculatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testTest(){
+        $this->assertTrue(true);
+    }
+}
